8699e8f6612179edbf1dbaeb665316785d99dfde
minsu6862 <chl6863@gmail.com>
2025-11-03 17:54:20 +0900

    자유게시판 기능 구현11

--- src/main/java/com/example/test/repository/FreeBoardRepository.java
+++ src/main/java/com/example/test/repository/FreeBoardRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long> {
 
     @Query("SELECT f FROM FreeBoard f JOIN FETCH f.user ORDER BY f.fWriteTime DESC")
