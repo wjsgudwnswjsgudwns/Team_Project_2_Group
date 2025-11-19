55676b67f8e61117268cdc6988427e3abedeb162
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-11-19 16:33:23 +0900

    배포 준비

---
+++ src/main/java/com/example/test/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.example.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = { "/", "/{path:^(?!api$).*$}/**" })
+    public String forward() {
+        return "forward:/index.html";
+    }
+
+}
